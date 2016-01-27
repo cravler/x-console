14b439529363d8615603a7745a06cfdb7b8f4e77
cravler <sergeizz@gmail.com>
2016-01-27 10:22:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var util = require('util');
+var colors = require('colors/safe');
+colors.enabled = true;
+
+module.exports = console;
+
+/**
+ * @param type
+ * @param color
+ * @param args
+ * @returns {Function}
+ */
+function console(type, color, args) {
+    if (!type) {
+        type = 'log';
+    }
+    if (Array.isArray(color)) {
+        args = color;
+        color = null;
+    }
+
+    const fn = function() {
+        const args = Array.prototype.slice.call(arguments);
+        if (color) {
+            args.unshift(colors[color](args.shift()));
+        }
+        global.console[type].apply(global.console, [util.format.apply(null, args)]);
+    };
+
+    if (args) {
+        fn.apply(null, args);
+    }
+
+    return fn;
+}
+
+console.log = console('log');
+console.info = console('info', 'blue');
+console.warn = console('warn', 'yellow');
+console.error = console('error', 'red');
